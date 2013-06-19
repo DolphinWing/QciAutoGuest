e7ce5e6aa50a731a075000d0b8cb10b21ff779e1
DolphinWing <dolphinwing74+github@gmail.com>
2013-06-20 00:38:50 +0800

    add Google AdMob Ads
    	modified:   AndroidManifest.xml
    	modified:   AutoGuest.iml
    	modified:   assets/Version.txt
    	new file:   res/layout/ad_layout.xml
    	modified:   res/xml/preference.xml
    	new file:   src/com/quanta/pobu/apps/AutoGuest/AdPreference.java

---
+++ src/com/quanta/pobu/apps/AutoGuest/AdPreference.java
@@ -0,0 +1,61 @@
+package com.quanta.pobu.apps.AutoGuest;
+
+import android.app.Activity;
+import android.content.Context;
+import android.preference.Preference;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.google.ads.AdRequest;
+import com.google.ads.AdSize;
+import com.google.ads.AdView;
+
+/**
+ * Created by dolphin on 2013/6/18.
+ *
+ * Google AdMob for Android
+ * https://developers.google.com/mobile-ads-sdk/docs/android/fundamentals?hl=zh-tw
+ *
+ * Android Admob advert in PreferenceActivity
+ * http://stackoverflow.com/a/5850299
+ *
+ * Android + AdMob
+ * http://imax-live.blogspot.tw/2012/11/android-sdk-eclipse-google-admob-ads.html
+ */
+public class AdPreference extends Preference {
+    public AdPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public AdPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AdPreference(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected View onCreateView(ViewGroup parent) {
+        // this will create the linear layout defined in ads_layout.xml
+        View view = super.onCreateView(parent);
+
+        // the context is a PreferenceActivity
+        Activity activity = (Activity) getContext();
+
+        // Create the adView
+        AdView adView = new AdView(activity, AdSize.BANNER, "YOUR-ADMOB-ID");
+
+        ((LinearLayout) view).addView(adView);
+
+        // Initiate a generic request to load it with an ad
+        AdRequest request = new AdRequest();
+        request.addTestDevice(AdRequest.TEST_EMULATOR);
+        adView.loadAd(request);
+
+        return view;
+    }
+
+}
